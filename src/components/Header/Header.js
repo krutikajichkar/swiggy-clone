a8bd1c6e6443eec5d8274679a43cd93ab0f84be2
Krutika Jichkar <93243210+krutikajichkar@users.noreply.github.com>
2023-08-02 21:48:04 +0530

    created file structure

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Header.css"
+
+const Header = () => {
+  return (
+    <div className='Header-wrapper'>
+        <div className="logo-wrapper">
+            <img className='logo' src="" alt="" />
+        </div>
+
+    </div>
+  )
+}
+
+export default Header
